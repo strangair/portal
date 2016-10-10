09fc366be02b618f6d370788130df4076d9d3b79
Pavel Varnavsky <varnavsky.pavel@gmail.com>
2016-10-10 12:01:12 +0300

    Sync

--- client/views.py
+++ client/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.template.loader import get_template
+from django.template import Context
 
-# Create your views here.
+def main_page(request):
+    t = get_template('client_main.html')
+    html = t.render(Context())
+    return HttpResponse(html)
