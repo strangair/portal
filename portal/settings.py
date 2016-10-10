317227f044be89bd12a5eefc70443a1d657828c9
Pavel Varnavsky <varnavsky.pavel@gmail.com>
2016-10-10 13:06:02 +0300

    Sync

--- portal/settings.py
+++ portal/settings.py
@@ -75,9 +75,16 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite',
+        'USER': 'django',
+        'PASSWORD': '123qweASD',
     }
 }
 
