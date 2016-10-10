5f3b91984b8323f8bfc90eb7ce7b2bb1317b7710
Pavel Varnavsky <varnavsky.pavel@gmail.com>
2016-10-10 11:50:19 +0300

    Sync

--- portal/settings.py
+++ portal/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'client',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'portal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/opt/www/django/portal/templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
