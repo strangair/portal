decc4fbc8d74fdfed24db6f6357d7f12d7b4288c
Pavel Varnavsky <varnavsky.pavel@gmail.com>
2016-10-18 12:56:29 +0300

    sync

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.shortcuts import render_to_response
 
-def registration(request):
+class AuthForm(forms.Form):
+    username = forms.EmailField(widget=forms.EmailInput)
+    password = forms.CharField(widget=forms.PasswordInput)
 
-    return None
+def registration(request):
+    if request.method == 'POST':
+        form = AuthForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = auth.authenticate(username=data['username'], password=data['password'])
+            if user is not None and user.is_active:
+                return HttpResponseRedirect('/client/')
+            else:
+                return render_to_response('login.html', {'error': form.errors})
+        else:
+            return render_to_response('login.html', {'error': form.errors})
