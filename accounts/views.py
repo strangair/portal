4eb62a99b7488abe389eaea3c80578dedea00b9c
Pavel Varnavsky <varnavsky.pavel@gmail.com>
2016-10-18 11:07:29 +0300

    sync

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,1 @@
 from django.shortcuts import render
-
-class AuthForm(forms.Form):
-    login = forms.CharField(widget=forms.EmailInput)
-    password = forms.CharField(widget=forms.PasswordInput)
-
-def login(request):
-    t = get_template('client_main.html')
-    html = t.render(Context())
-    return HttpResponse(html)
