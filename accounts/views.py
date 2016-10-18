27bfdf75cc0ce4296d74c91299b1f4915ad6a38c
Pavel Varnavsky <varnavsky.pavel@gmail.com>
2016-10-18 16:13:58 +0300

    sync

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import auth
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.shortcuts import render_to_response
 
